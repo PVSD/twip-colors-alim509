76bc17384d48caccfdda4b0e88d4603b8bd24c9b
al5091 <angelovinolim@gmail.com>
2019-01-28 11:21:58 -0500

    colors are wack

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter in three values to represent an RGB triplet.");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        a = Integer
+
     }
 }
