efc8fe7c18e96d3cf338152ee63ed70f0a3d5652
Angelo Lim <angelovinolim@gmail.com>
2019-01-31 20:05:34 -0500

    Crayola

---
+++ src/com/company/Colors.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Colors {
+
+    private String color;
+    private String hex;
+
+    public Colors(String c, String h) {
+        color = c;
+        hex = h;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getHex() {
+        return hex;
+    }
+
+}
